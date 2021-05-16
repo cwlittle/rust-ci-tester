b2f0b7cdce010d20585a240e6fc781ce5f2b67a0
Charlie Little <cwlittle@utexas.edu>
2021-05-16 05:22:57 -0500

    Add test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     get_hash("Hello, Earthlings!");
-    add(3,4);
+    add_them(3,4);
 }
 
 fn get_hash(input: &str) -> blake3::Hash {
@@ -10,6 +10,19 @@ fn get_hash(input: &str) -> blake3::Hash {
     hash2
 }
 
-fn add(x: i32, y:i32) -> i32 {
+pub fn add_them(x: i32, y:i32) -> i32 {
+    if x == 3 {
+        return 42;
+    }
     x + y
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        assert_eq!(add_them(2, 2), 4);
+    }
 }
